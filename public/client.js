188276d307c06b8ce57b581234f2e1754d6a26cd
Hrishikesh Bharadkar <hrishikeshb@acs.com>
2020-10-17 15:17:18 +0530

    changes for turn1

--- public/client.js
+++ public/client.js
@@ -14,7 +14,7 @@ var iceServers={
 	'iceServers':[
 		{'url':'stun:stun.services.mozilla.com'},
 		{'url':'stun:stun.l.google.com:19302'},
-     {'urls': 'turn:192.168.100.187:3478?transport=tcp',
+     {'urls': 'turn:120.138.121.50:3478?transport=tcp',
     'credential': 'test',
     'username': 'test'
     }    

24321e7f463917c891e08b4cbd95161bf70849e3
Hrishikesh Bharadkar <hrishikeshb@acs.com>
2020-11-05 09:03:07 +0530

    images autosave for heroku

--- public/client.js
+++ public/client.js
@@ -250,7 +250,7 @@ var video = document.getElementById('remoteVideo')
     }, false);
 
     document.getElementById('startbutton').addEventListener('click', function(ev){
-      url = "https://vkap-deploy.herokuapp.com/uploadPicture";
+      url = "http://localhost:3000/uploadPicture";
       upload_picture(url);
       ev.preventDefault();
     }, false);
@@ -324,7 +324,7 @@ var video = document.getElementById('remoteVideo')
   ///////////////////////////////////capture Pan image/////////////////////////////////////////////////
 var video1 = document.getElementById('remoteVideo')
 document.getElementById('pan').addEventListener('click', function(ev){
-  url1 = "https://vkap-deploy.herokuapp.com/uploadPicture1";
+  url1 = "http://localhost:3000/uploadPicture1";
       upload_picture1(url1);
       ev.preventDefault();
     }, false);
@@ -390,7 +390,7 @@ clearphoto1();
 
   var video1 = document.getElementById('remoteVideo')
 document.getElementById('Signature').addEventListener('click', function(ev){
-  url2 = "https://vkap-deploy.herokuapp.com/uploadPicture2";
+  url2 = "https://localhost:3000/uploadPicture2";
       upload_picture2(url2);
       ev.preventDefault();
     }, false);
